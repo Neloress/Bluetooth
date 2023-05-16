05e3714b407507280ed362dcde32d5b2b7a4100e
Neloress <60485477+Neloress@users.noreply.github.com>
2023-05-16 18:15:24 +0200

    autoscan

--- AndroidBluetooth/MainPage.xaml.cs
+++ AndroidBluetooth/MainPage.xaml.cs
@@ -1,46 +1,77 @@
-﻿//using Android.OS;
-//using AndroidBluetooth.Platforms.Android;
-//using AndroidX.Core.App;
-//using AndroidX.Core.Content;
-using Plugin.BLE;
+﻿using Plugin.BLE;
 using Plugin.BLE.Abstractions.Contracts;
-//using System;
-//using System.Runtime.Intrinsics.X86;
+using System;
 
 namespace AndroidBluetooth;
 
 public partial class MainPage : ContentPage
 {
-	private IDevice Tag = null;
+	//private IDevice Tag = null;
+	private List<Tuple<long, int>> _values;
+	private bool _runSearch;
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
+	private async void CollectData(int stepDuration,List<Tuple<long,int>> values)
+	{
+		var ble = CrossBluetoothLE.Current;
+		var adapter = CrossBluetoothLE.Current.Adapter;
+
+
+		List<IDevice> deviceList = new List<IDevice>();
+
+		adapter.ScanTimeout = stepDuration;
+		adapter.ScanMode = ScanMode.LowLatency;//ScanMode.Passive;
+		adapter.DeviceDiscovered += (s, a) => deviceList.Add(a.Device);
+
+		while (_runSearch) 
+		{
+			//deviceList = new List<IDevice>();
+			//adapter.DeviceDiscovered += (s, a) => deviceList.Add(a.Device);
+			await adapter.StartScanningForDevicesAsync();
+
+			foreach (IDevice device in deviceList)
+			{
+				if (device.Name != null && device.Name.Contains("Smart Tag"))
+				{
+					int value = device.Rssi;
+					long time = System.DateTime.Now.Ticks;
+					_values.Add(new Tuple<long, int>(time,value));
+				}
+			}
+		}
+	}
 	private async void OnCounterClicked(object sender, EventArgs e)
 	{
-		if (Tag != null)
-		{
-			var rssiUpdateResult = await Tag.UpdateRssiAsync();
-			int value = Tag.Rssi;
+		//_runSearch = false;
 
-			CounterBtn.Text = $""+value;
-		}
-		else
-		{
-			CounterBtn.Text = $"No Tag";
-		}
+		//if (Tag != null)
+		//{
+			
+
+		//	//while (!await Tag.UpdateRssiAsync()) ;
+
+		//	bool rssiUpdateResult = await Tag.UpdateRssiAsync();
+		//	int value = Tag.Rssi;
+
+		//	CounterBtn.Text = $""+value;
+		//}
+		//else
+		//{
+		//	CounterBtn.Text = $"No Tag";
+		//}
 		
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		//SemanticScreenReader.Announce(CounterBtn.Text);
 	}
 
 	private async Task<bool> CheckForBluetoothPermission()
 	{
-#if ANDROID
-		var status = await Permissions.CheckStatusAsync<Permissions.LocationAlways>();//MyBluetoothPermission
-
+		var status = await Permissions.CheckStatusAsync<Permissions.LocationAlways>();
+		
 		if (status == PermissionStatus.Granted)
 			return true;
 
@@ -52,96 +83,91 @@ public partial class MainPage : ContentPage
 		status = await Permissions.RequestAsync<Permissions.LocationAlways>();
 
 		return status == PermissionStatus.Granted;
-#endif
 	}
-	public int Rssi { [Android.Runtime.Register("getRssi", "()I", "")] get; }
 
-	private async void BluetoothDetection_Clicked(object sender, EventArgs e)
+	private async Task<Tuple<bool,int>> Measure(int timeOut)
 	{
-#if ANDROID
-		if (!await CheckForBluetoothPermission())
-			return;
-
 		var ble = CrossBluetoothLE.Current;
 		var adapter = CrossBluetoothLE.Current.Adapter;
 
-		//var state = ble.State;
 
-		//List<IDevice> deviceList1 = new List<IDevice>();
-		List<IDevice> deviceList2 = new List<IDevice>();
+		List<IDevice> deviceList = new List<IDevice>();
 
-		adapter.ScanTimeout = 10000;
-		adapter.ScanMode = ScanMode.Passive;
+		adapter.ScanTimeout = timeOut;
+		adapter.ScanMode = ScanMode.LowLatency;//ScanMode.Passive;
 
-		
-		
-		//adapter.DeviceAdvertised += (s, a) => deviceList1.Add(a.Device);
-		adapter.DeviceDiscovered += (s, a) => deviceList2.Add(a.Device);
+
+
+		adapter.DeviceDiscovered += (s, a) => deviceList.Add(a.Device);
 		await adapter.StartScanningForDevicesAsync();
 
-		//List<IDevice> asd = adapter.DiscoveredDevices.ToList();
 
-		
 
-		foreach (IDevice device in deviceList2)
+
+		foreach (IDevice device in deviceList)
 		{
-			if (device.Name.Contains("Smart Tag"))
+			if (device.Name != null && device.Name.Contains("Smart Tag"))
 			{
-				Tag = device;
+				return new Tuple<bool, int>(true, device.Rssi);
+			}
+		}
+		return new Tuple<bool, int>(false, -1);
+	}
 
-				var rssiUpdateResult = await device.UpdateRssiAsync();
-				int value = device.Rssi;
+	private async void BluetoothDetection_Clicked(object sender, EventArgs e)
+	{
+		if (!await CheckForBluetoothPermission())
+			throw new Exception("No Permision");
 
-				int sdrf = 0;
+		//_runSearch = true;
+
+		//_values = new List<Tuple<long, int>>();
+		//Thread thread = new Thread(() => CollectData(1000, _values));
+		//thread.Start();
+
+		//Thread.Sleep(100000);
+		//_runSearch = false;
+		//thread.Join();
+
+		//int stop = 56;
+
+		//var ble = CrossBluetoothLE.Current;
+		//var adapter = CrossBluetoothLE.Current.Adapter;
+
+
+		//List<IDevice> deviceList2 = new List<IDevice>();
+
+		//adapter.ScanTimeout = 1000;
+		//adapter.ScanMode = ScanMode.LowLatency;//ScanMode.Passive;
+
+
+
+		////adapter.DeviceAdvertised += (s, a) => deviceList1.Add(a.Device);
+		//adapter.DeviceDiscovered += (s, a) => deviceList2.Add(a.Device);
+		//await adapter.StartScanningForDevicesAsync();
+
+
+
+
+		//foreach (IDevice device in deviceList2)
+		//{
+		//	if (device.Name != null && device.Name.Contains("Smart Tag"))
+		//	{
+		//		int stop3 = 23;// Tag = device;
+		//	}
+		//}
+
+		List<Tuple<long, int>> values = new List<Tuple<long, int>>();
+		for (int i = 0; i < 10; i++)
+		{
+			Tuple<bool, int> tuple = await Measure(10000);
+			if (tuple.Item1)
+			{
+				long time = System.DateTime.Now.Ticks;
+				values.Add(new Tuple<long, int>(time,tuple.Item2));
 			}
 		}
 
-#endif
-		//#if ANDROID
-
-		//		if (!await CheckForBluetoothPermission())
-		//			return;
-
-		//		var adapter = CrossBluetoothLE.Current.Adapter;
-
-		//		adapter.DeviceAdvertised += Adapter_DeviceAdvertised;
-		//		adapter.DeviceConnected += Adapter_DeviceConnected;
-		//		adapter.DeviceConnectionLost += Adapter_DeviceConnectionLost;
-		//		adapter.DeviceDisconnected += Adapter_DeviceDisconnected;
-		//		adapter.DeviceDiscovered += Adapter_DeviceDiscovered;
-		//		adapter.ScanTimeoutElapsed += Adapter_ScanTimeoutElapsed;
-		//		await adapter.StartScanningForDevicesAsync();
-		//#endif
 	}
-
-	//private void Adapter_ScanTimeoutElapsed(object sender, EventArgs e)
-	//{
-	//	Console.WriteLine("ScanTimeoutElapsed");
-	//}
-
-	//private void Adapter_DeviceDiscovered(object sender, Plugin.BLE.Abstractions.EventArgs.DeviceEventArgs e)
-	//{
-	//	Console.WriteLine("DeviceDiscovered");
-	//}
-
-	//private void Adapter_DeviceDisconnected(object sender, Plugin.BLE.Abstractions.EventArgs.DeviceEventArgs e)
-	//{
-	//	Console.WriteLine("DeviceDisconnected");
-	//}
-
-	//private void Adapter_DeviceConnectionLost(object sender, Plugin.BLE.Abstractions.EventArgs.DeviceErrorEventArgs e)
-	//{
-	//	Console.WriteLine("DeviceConnectionLost");
-	//}
-
-	//private void Adapter_DeviceConnected(object sender, Plugin.BLE.Abstractions.EventArgs.DeviceEventArgs e)
-	//{
-	//	Console.WriteLine("DeviceConnected");
-	//}
-
-	//private void Adapter_DeviceAdvertised(object sender, Plugin.BLE.Abstractions.EventArgs.DeviceEventArgs e)
-	//{
-	//	Console.WriteLine("DeviceAdvertised");
-	//}
 }
 
